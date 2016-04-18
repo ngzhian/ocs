6dbc43ff308c57c7ffc282697f19d792024a8bf1
Ng Zhi An <ngzhian@gmail.com>
2016-04-18 02:05:07 +0000

    Use jingoo for templating

--- src/ocs.ml
+++ src/ocs.ml
@@ -10,8 +10,13 @@ type post =
 
 type site =
   { base_dir: string;
+    post_paths : string list;
     posts : post list;
     template: string;
+  }
+
+type config =
+  { dir: string;
   }
 
 let get_title md = 
@@ -23,6 +28,13 @@ let get_title md =
   )
   | _ -> ""
 
+let is_md path =
+  String.is_suffix path ~suffix:".md"
+
+let search_for_mds dir =
+  Sys.ls_dir dir
+  |> List.filter ~f:is_md
+
 let md_to_post path =
   let contents = In_channel.read_all path in
   let md = Omd.of_string contents in
@@ -31,31 +43,59 @@ let md_to_post path =
   let html_path = String.chop_suffix_exn path ~suffix:".md" ^ ".html" in
   { path; contents; html; title; html_path; }
 
-let list_dir dir =
-  let is_md path = String.is_suffix path ~suffix:".md" in
-  Sys.ls_dir dir
-  |> List.filter ~f:is_md
+let jg_template template post =
+  let open Jg_types in
+  let pt = Tobj [("content", Tstr post.html); ("title", Tstr post.title)] in
+  Jg_template.from_string template ~models:[("post", pt)]
 
 let apply_template template post =
-  { post with html = 
-      Str.replace_first (Str.regexp "{{BODY}}") post.html template
-      |> Str.replace_first (Str.regexp "{{TITLE}}") post.title }
-
-let all_html dir template = 
-  list_dir dir
-  |> List.map ~f:md_to_post
-  |> List.map ~f:(apply_template template)
+  { post with html =
+      jg_template template post
+  }
 
 let write site =
   let write_html post = Out_channel.write_all post.html_path ~data:post.html in
   List.iter site.posts ~f:write_html
   
-let build_site dir =
-  let template_path = Filename.concat dir "post.html" in
+let build_site config =
+  let template_path = Filename.concat config.dir "post.html" in
   let template = In_channel.read_all template_path in
-  let posts = all_html dir template in
-  { base_dir = dir; posts; template; }
+  let post_paths = search_for_mds config.dir in
+  { base_dir = config.dir; post_paths; template; posts = []}
+
+let convert site =
+  let posts = 
+    site.post_paths
+    |> List.map ~f:md_to_post
+    |> List.map ~f:(apply_template site.template) in
+  {site with posts}
   
-let generate dir =
-  build_site dir
+let generate config =
+  build_site config
+  |> convert
   |> write
+
+let default_config = 
+  { dir = "."
+  }
+
+let generate_default () =
+  generate default_config
+
+let generate_with_dir dir =
+  generate { dir }
+
+(**
+ *  Command line interface to ocs.
+ *)
+let () =
+  Command.run ~version:"1.0" (
+    Command.basic 
+    ~summary:"Generate a static blog from markdown"
+    Command.Spec.(
+      empty
+      +> flag "-d" (optional string) ~doc:"directory"
+    )
+    (fun dir () -> match dir with
+       | Some path -> generate_with_dir path
+       | None -> generate_default () ))
--- src/ocs.mli
+++ src/ocs.mli
@@ -10,22 +10,25 @@ type post =
 
 type site =
   { base_dir: string;
+    post_paths : string list;
     posts : post list;
     template: string;
   }
 
+type config =
+  { dir: string;
+  }
+
 val md_to_post : string -> post
-val list_dir : string -> string list
+val search_for_mds : string -> string list
 
-(* (md file path * html) *)
-val all_html : string -> string -> post list
-val write : site -> unit
-
-val generate : string -> unit
+val generate : config -> unit
+val generate_default : unit -> unit
 
 val apply_template : string -> post -> post
 
-(* val tt : Omd_representation.element -> string *)
 val get_title : Omd.t -> string
 
-val build_site : string -> site
+val build_site : config -> site
+val convert : site -> site
+val write : site -> unit
